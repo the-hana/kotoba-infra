843299516d27f127aa97390751be3d2d3d02993e
ButAhana <thebutahana@gmail.com>
2026-06-28 23:10:01 +0900

    fix: Lambda webhookにX-Internal-Token認証ヘッダーを追加
    RailsのWebhook認証に対応してLambdaがSSMからINTERNAL_API_KEYを
    取得してX-Internal-Tokenヘッダーに付与するよう変更。
    ECSもSSM secrets injectionで同じキーを受け取る構成に統一した。

--- lambda/daily_story.py
+++ lambda/daily_story.py
@@ -1,14 +1,24 @@
 import os
 import urllib.request
 import urllib.error
+import boto3
+
+ssm = boto3.client("ssm")
+
+
+def _get_internal_token():
+    name = os.environ["INTERNAL_API_KEY_PARAM"]
+    res = ssm.get_parameter(Name=name, WithDecryption=True)
+    return res["Parameter"]["Value"]
 
 
 def handler(event, context):
     url = os.environ["WEBHOOK_URL"]
+    token = _get_internal_token()
     req = urllib.request.Request(
         url,
         data=b"{}",
-        headers={"Content-Type": "application/json"},
+        headers={"Content-Type": "application/json", "X-Internal-Token": token},
         method="POST",
     )
     try:
